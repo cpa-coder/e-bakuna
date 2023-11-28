84997de7fcca1dca8ead46828fa490207ad17922
Ogie Galicia <o.galicia.cpa@gmail.com>
2023-11-28 15:25:44 +0800

    wip

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,21 +1,14 @@
 <script lang="ts">
 	import './styles.css';
-	import { onMount, beforeUpdate, afterUpdate } from 'svelte';
 	import { Icon } from '@smui/common';
 	import { mdiMenu, mdiWhiteBalanceSunny, mdiWeatherNight } from '@mdi/js';
 	import IconButton from '@smui/icon-button';
 	import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
 
 	let topAppBar: TopAppBar;
-
 	let darkTheme: boolean | undefined = undefined;
-	let themeIcon: string;
 
-	$: {
-		themeIcon = darkTheme ? mdiWhiteBalanceSunny : mdiWeatherNight;
-	}
-
-	onMount(() => {
+	if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
 		const storedTheme = localStorage.getItem('theme');
 
 		if (storedTheme === null) {
@@ -23,11 +16,13 @@
 		} else {
 			darkTheme = storedTheme === 'dark';
 		}
-	});
+	}
 
-	function updateTheme() {
-		darkTheme = !darkTheme;
-		localStorage.setItem('theme', darkTheme ? 'dark' : 'light');
+	function toggleTheme() {
+		if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+			localStorage.setItem('theme', darkTheme ? 'light' : 'dark');
+			darkTheme = !darkTheme;
+		}
 	}
 </script>
 
@@ -41,28 +36,34 @@
 	{:else}
 		<link rel="stylesheet" href="/smui.css" />
 	{/if}
-	<script src="/startup.js"></script>
 </svelte:head>
+<div class="app-transition">
+	<TopAppBar bind:this={topAppBar} variant="standard">
+		<Row>
+			<Section>
+				<IconButton>
+					<Icon tag="svg" viewBox="0 0 24 24">
+						<path fill="currentColor" d={mdiMenu} />
+					</Icon>
+				</IconButton>
+				<Title>E-Bakuna</Title>
+			</Section>
+			<Section align="end" toolbar>
+				<IconButton on:click={toggleTheme}>
+					<Icon tag="svg" viewBox="0 0 24 24">
+						<path fill="currentColor" d={darkTheme ? mdiWhiteBalanceSunny : mdiWeatherNight} />
+					</Icon>
+				</IconButton>
+			</Section>
+		</Row>
+	</TopAppBar>
+	<AutoAdjust {topAppBar}>
+		<slot />
+	</AutoAdjust>
+</div>
 
-<TopAppBar bind:this={topAppBar} variant="standard">
-	<Row>
-		<Section>
-			<IconButton>
-				<Icon tag="svg" viewBox="0 0 24 24">
-					<path fill="currentColor" d={mdiMenu} />
-				</Icon>
-			</IconButton>
-			<Title>E-Bakuna</Title>
-		</Section>
-		<Section align="end" toolbar>
-			<IconButton on:click={updateTheme}>
-				<Icon tag="svg" viewBox="0 0 24 24">
-					<path fill="currentColor" d={themeIcon} />
-				</Icon>
-			</IconButton>
-		</Section>
-	</Row>
-</TopAppBar>
-<AutoAdjust {topAppBar}>
-	<slot />
-</AutoAdjust>
+<style>
+	.app-transition {
+		transition: all 0.2s ease-in-out;
+	}
+</style>
